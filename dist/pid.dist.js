64766044507705fe41ce539df14e0008023b47bf
nanasnumber <nanasnumber@gmail.com>
2022-09-23 22:20:55 -0400

    Add build script

---
+++ dist/pid.dist.js
@@ -0,0 +1,60 @@
+
+
+export default class piq extends HTMLElement {
+
+  constructor() {
+    // Use super to access decendent's context
+    const s = super()
+
+    // Set Style
+    this.setStyle = () => {
+      const id = `style_${s.name()}`;;
+      const css = s.style();
+      const styleNode = document.getElementById(id);
+
+      if (styleNode) {
+        return;
+      }
+
+      const style = document.createElement('style');
+      style.id = id;
+      style.innerHTML = css;
+      document.head.appendChild(style);
+    };
+
+    // Render method for compoenent
+    this.render = async () => {
+
+      this.innerHTML = await s.template();
+
+      if (s.connected !== undefined) {
+        await s.connected();
+      }
+    };
+
+  };
+
+  props(s) {
+    // return attribute data
+    return this.getAttribute(s);
+  };
+
+  static get observedAttributes() {
+    return this.attr();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (newValue === oldValue) {
+      return;
+    }
+    this.render();
+  }
+
+  connectedCallback() {
+    this.setStyle();
+    this.render();
+  };
+};
+
+
+
---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf dist
+mkdir dist
+cp ./src/core/piq.js ./dist/pid.dist.js
